66e480b65aa9d35a26ad46c995e73b6f91421a12
peang <pngo93@gmail.com>
2024-09-14 14:19:04 -0500

    home page update

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,14 @@
 // src/pages/Home.tsx
 import React from 'react';
-// import IdeaCard from '../components/IdeaCard';
+import IdeaForm from '../components/IdeaForm';
+import IdeaList from '../components/IdeaList';
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Vote on Ideas</h1>
-      {/* IdeaCard components will go here */}
-      {/* <IdeaCard />
-      <IdeaCard /> */}
-      {/* Add more IdeaCards as needed */}
+      <IdeaForm />
+      <IdeaList />
     </div>
   );
 };
